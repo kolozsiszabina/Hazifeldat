05d7e1e9d9a0aa8e673b1eaa2522edfc595231d8
kolozsiszabina <36516495+kolozsiszabina@users.noreply.github.com>
2018-02-25 20:29:14 +0100

    Megcsináltam 8-20 feladatot

--- beadando1.py
+++ beadando1.py
@@ -53,6 +53,121 @@ def feladat_7(a,b, dh):
     else:
         kell_meg=Kt-dh
         print('Meg', kell_meg, 'meter hosszu drot kell.')
+        
+def feladat_8(a,b,c,d,x):
+    def f_fuggveny():
+        if x<5:
+            return 3*x-5
+        elif x>=5 and x<=10:
+            return 10
+        elif x>10:
+            return 9*x+1
+    def E():
+        if a+c>2*d and b>0:
+            return d-3*b
+        elif a+c<2*d and b<0:
+            return d+3*b
+        else:
+            return 4
+
+    print('x=',f_fuggveny(), 'E=', E())
+
+def feladat_10(a,b):
+    ossz=b-a
+    db=0
+    for i in range(0,ossz):
+        if i%100==0 and i%400==0 or i%100!=0 and i%4==0:
+            db+=1
+
+    print(db)
+
+def feladat_12(max_pont, elert_pont):
+    if elert_pont>=max_pont*0.60:
+        print('Sikeres vizsga.')
+    else:
+        print('Sikertelen vizsga.')
+
+def feladat_13(erdemjegy):
+    if erdemjegy==1:
+        print('Elegtelen')
+    elif erdemjegy==2:
+        print('Elegseges')
+    elif erdemjegy==3:
+        print('Kozepes')
+    elif erdemjegy==4:
+        print('Jo')
+    elif erdemjegy==5:
+        print('Jeles')
+
+def feladat_14(honap_szama):
+    if honap_szama==1:
+        print('Januar')
+    elif honap_szama==2:
+        print('Februar')
+    elif honap_szama==3:
+        print('Marcius')
+    elif honap_szama==4:
+        print('Aprilis')
+    elif honap_szama==5:
+        print('Majus')
+    elif honap_szama==6:
+        print('Junius')
+    elif honap_szama==7:
+        print('Julius')
+    elif honap_szama==8:
+        print('Augusztus')
+    elif honap_szama==9:
+        print('Szeptember')
+    elif honap_szama==10:
+        print('Oktober')
+    elif honap_szama==11:
+        print('November')
+    elif honap_szama==12:
+        print('December')
+
+def feladat_15(a,b):
+    hanyados=0
+    while a>=b:
+        hanyados+=1
+        a=a-b
+    print(hanyados)
+
+def feladat_16(a,b):
+    while True:
+        r=a%b
+        a=b
+        b=r
+        if r==0:
+            break
+    return a
+
+def feladat_17(x):
+    masolat=x
+    uj_szam=0
+    while x!=0:
+        szamjegy=x%10
+        uj_szam=(uj_szam*10)+szamjegy
+        x=x//10
+    return uj_szam==masolat
+
+def feladat_19(n):
+    if n<2:
+        return False
+    else:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+        return True
+
+def feladat_20(n):
+    a=1
+    b=1
+    k=1
+    while k<n:
+        print('%.4f'%(a/b), end=', ')
+        a=a+b
+        b=a-b
+        k+=1
 
 
 def main():
@@ -63,5 +178,15 @@ def main():
     feladat_5(2,3,5,0)
     feladat_6(2,3,4)
     feladat_7(5,7,5)
+    feladat_8(1, 2, 3, 4, 5)
+    feladat_10(1998, 2018)
+    feladat_12(100, 60)
+    feladat_13(3)
+    feladat_14(5)
+    feladat_15(520, 104)
+    print(feladat_16(360, 225))
+    print(feladat_17(353))
+    print(feladat_19(5))
+    feladat_20(5)
 if __name__ == '__main__':
     main()
